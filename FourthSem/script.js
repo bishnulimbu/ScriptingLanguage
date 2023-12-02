8cf5824e0569599db92ed2a2cf14b09237fb0c1f
bishnulimbu <subashlimbu987654321@gmail.com>
2023-12-02 16:50:18 +0545

    hh

---
+++ FourthSem/script.js
@@ -0,0 +1,8 @@
+console.log("Hello Wordl");
+let boy1, boy2 , boy3;
+boy1 = "ram";
+boy2 = "sam";
+boy3 = "hari";
+console.log(boy1);
+console.log(boy2);
+console.log(boy3);
---
+++ FourthSem/script1.js
@@ -0,0 +1,9 @@
+let x,y, z;
+x = 10;
+y = 20;
+z = x + y;
+let userName;
+userName= 'ram' + ' ' + 'dash'+ ' ' + 'dash' + ' ' + 'hari';
+console.log(z);
+console.log(userName);
+
