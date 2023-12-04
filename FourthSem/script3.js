53e59a66b99c74605ba58dff4bb6a5cee173bfc1
bishnulimbu <subashlimbu987654321@gmail.com>
2023-12-04 07:17:52 +0545

    asdfasd

---
+++ FourthSem/script3.js
@@ -0,0 +1,12 @@
+let text1 = "ram";
+let text2 = "ram";
+let text3 = text1 === text2;
+
+let a = 10 + 10;
+let b = "10" + 10;
+let c = "10" + "10";
+console.log(text3);
+console.log(a);
+console.log(b);
+console.log(c);
+
