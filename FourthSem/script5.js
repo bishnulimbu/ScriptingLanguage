e27e31e65edaa1c07151d23be1745d8135f65c7b
bishnulimbu <subashlimbu987654321@gmail.com>
2023-12-04 07:34:22 +0545

    dfadf

---
+++ FourthSem/script5.js
@@ -0,0 +1,15 @@
+let a = 5;
+let b = 10;
+
+console.log("AND operator");
+console.log(a == 5 && b == 10);
+console.log(a == 1 && b == 10);
+console.log(a == 5 && b == 2);
+console.log(a == 12 && b == 11);
+console.log("OR operator");
+console.log(a == 5 || b == 10);
+console.log(a == 1 || b == 10);
+console.log(a == 5 || b == 2);
+console.log(a == 12 || b == 11);
+console.log("NOT operator");
+console.log(!(a == 5 === b == 10));
