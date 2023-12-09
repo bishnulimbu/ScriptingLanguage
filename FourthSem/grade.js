679d951c30abc35e1b750d59afcb2333f7fe33fd
bishnulimbu <subashlimbu987654321@gmail.com>
2023-12-10 01:35:20 +0545

    practice

---
+++ FourthSem/grade.js
@@ -0,0 +1,23 @@
+let percent = prompt("enter you percentage");
+
+if(percent >= 90){
+    console.log("A+ Grade");
+}
+else if(percent >= 80){
+    console.log("A Grade");
+}
+else if(percent >= 70){
+    console.log("B+ Grade");
+}
+else if(percent >= 60){
+    console.log("B Grade");
+}
+else if(percent >= 50){
+    console.log("C+ Grade");
+}
+else if(percent > 40){
+    console.log("C Grade");
+}
+else{
+    console.log("you failed.")
+}
