17bcd94b8faeec22ccb992c4f61a0020b82108ba
bishnulimbu <subashlimbu987654321@gmail.com>
2023-12-18 07:11:16 +0545

    prime number js

--- FourthSem/primenumber.js
+++ FourthSem/primenumber.js
@@ -1,23 +1,42 @@
-function range(start, end) {
-    const primes = [];
+// function range(start, end) {
+//     const primes = [];
   
-    for (let num = start; num <= end; num++) {
-      let isPrime = true;
+//     for (let num = start; num <= end; num++) {
+//       let isPrime = true;
   
-      for (let i = 2; i <= Math.sqrt(num); i++) {
-        if (num % i === 0) {
-          isPrime = false;
-          break;
-        }
-      }
+//       for (let i = 2; i <= Math.sqrt(num); i++) {
+//         if (num % i === 0) {
+//           isPrime = false;
+//           break;
+//         }
+//       }
   
-      if (isPrime && num > 1) {
-        primes.push(num);
-      }
-    }
+//       if (isPrime && num > 1) {
+//         primes.push(num);
+//       }
+//     }
   
-    return primes;
+//     return primes;
+//   }
+  
+//   const primeNumbers = range(1, 100);
+//   console.log("Prime numbers between 1 and 100:", primeNumbers);
+function isPrime(num){
+  if(num<=1){
+    return false;
   }
-  
-  const primeNumbers = range(1, 100);
-  console.log("Prime numbers between 1 and 100:", primeNumbers);
+  for (let i = 2; i<=Math.sqrt(num);i++){
+    if(num%i == 0){
+      return false;
+    }
+  }
+  return true;
+}
+function displayPrime(start,end){
+  for(let i = start; i<=end; i++){
+    if(isPrime(i)){
+      console.log(i);
+    }
+  }
+}
+displayPrime(1,100);
