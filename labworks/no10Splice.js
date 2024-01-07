dbab1575c6018423a965d191afadbf5640b8cf52
bishnulimbu <subashlimbu987654321@gmail.com>
2024-01-07 16:19:05 +0545

    splice function usuage

---
+++ labworks/no10Splice.js
@@ -0,0 +1,10 @@
+const books = [
+  {bookName : 'American Psycho', author : 'Bret Easton Ellis'},
+  {bookName : 'Call of Chuthulu', author : 'HP Lovecraft'},
+  {bookName : 'Frankenstein', author : 'Mary Shelley'},
+];
+
+console.log(books);
+books.splice(2,2);
+console.log(books);
+
