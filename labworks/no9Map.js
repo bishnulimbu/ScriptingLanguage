b8247b809770c30a4e27b5e6a703ebc3b9eb23d7
bishnulimbu <subashlimbu987654321@gmail.com>
2024-01-07 16:03:55 +0545

    map usuage

---
+++ labworks/no9Map.js
@@ -0,0 +1,15 @@
+const arr1 = ['ramu', 'samu', 'hariu'];
+console.log(arr1);
+
+const arr2 = arr1.map(function (nam){
+return nam.toUpperCase();
+});
+console.log(arr2);
+
+
+
+// const one = [1,2,3,4,5];
+// const two = one.map(function (ram){
+//   return ram+ 'sam'; 
+// });
+// console.log(two);
