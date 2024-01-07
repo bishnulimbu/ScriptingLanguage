bf1315a64cb4eac67f40af3a6cc54805dfad1286
bishnulimbu <subashlimbu987654321@gmail.com>
2024-01-07 06:50:25 +0545

    sdafasdf

---
+++ practice/forofloop2.js
@@ -0,0 +1,17 @@
+const stdName = ['ram', 'sam', 'hari'];
+
+ stdName.forEach(myFunction);
+
+function myFunction(value, index){
+  console.log(index, ": ", value);
+}
+
+const num = [1,2,3,4,5,6];
+
+num.forEach(number =>{
+  console.log(number+2);
+})
+
+stdName.push('roshan');
+console.log(stdName);
+stdName.map
