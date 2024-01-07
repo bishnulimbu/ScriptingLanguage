baf7d2638f5f8c6d7a90ec2ca397552d0d0d40f4
bishnulimbu <subashlimbu987654321@gmail.com>
2024-01-07 07:08:36 +0545

    sorting in array

---
+++ FourthSem/array/array4.js
@@ -0,0 +1,9 @@
+const array1 = [
+  1, 2, 3, 4, 5
+];
+const array2 = [
+  'zebra', 'jockey', 'beavers', 'alps'
+]
+console.log(array1.length);
+console.log(array1.sort());
+console.log(array2.sort());
