60879908ab4fa516b8fbbb2f8a61b2ab74fc9a0c
bishnulimbu <subashlimbu987654321@gmail.com>
2024-01-07 06:50:30 +0545

    adfasdf

---
+++ practice/foreach.js
@@ -0,0 +1,10 @@
+let ram1 = ['ram1', 'ram2', 'ram3'];
+
+ram1.forEach((name, x)=>{
+  console.log(name)
+  console.log(x+2);
+});
+
+// ram1.forEach(function (value,x){
+//  console.log(value)
+// })
