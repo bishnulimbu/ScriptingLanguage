423a5c33d6c1cc09a87436d1744bdd4e425d1564
bishnulimbu <subashlimbu987654321@gmail.com>
2024-01-07 07:35:03 +0545

    dfad

---
+++ FourthSem/array/array5.js
@@ -0,0 +1,17 @@
+const array = [
+  'zebra', 'jockey', 'beavers', 'alps'
+];
+
+// for (i = 0; i < array.length; i++){
+//   console.log(array[i]);
+//   }
+
+// array.forEach(function (nam){
+//   console.log(nam +='animal');
+// }); 
+// array.forEach(fucntion (funame){}
+
+array.map(function (nam){
+  let a = (nam);
+  console.log(a);
+}); 
