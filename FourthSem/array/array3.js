8627c50ebe8ac8a6b49791e186e0c431b9440c5b
bishnulimbu <subashlimbu987654321@gmail.com>
2024-01-07 07:02:07 +0545

    dfasdf

---
+++ FourthSem/array/array3.js
@@ -0,0 +1,10 @@
+const apps = [];
+apps[0] = 'app1';
+apps[1] = 'app2';
+apps[2] = 'app3';
+apps[3] = 'app4';
+
+console.log(apps);
+
+const cars = new Array('car1', 'car2', 'car3', 'car4');
+console.log(cars);
