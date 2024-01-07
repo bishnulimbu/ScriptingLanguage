db2cd7c8d530ea9dfbe17b67fe728c6def5b1aea
bishnulimbu <subashlimbu987654321@gmail.com>
2024-01-07 06:50:29 +0545

    asdfasdf

---
+++ practice/forinloop.js
@@ -0,0 +1,17 @@
+let employee = {
+  name : 'ram',
+  age : 22,
+  job : 'engeener',
+  address : 'rampur' 
+
+} ;
+
+for (let key in employee){
+  console.log(key, ": ", employee[key])
+}
+
+//for let value of string
+// for (let avl of strVar){
+// do work
+// console.log(val)
+// };
