97b9f728dfaae52678e29254b0204492206e16c8
bishnulimbu <subashlimbu987654321@gmail.com>
2024-01-07 15:48:58 +0545

    push and pop in an array

---
+++ labworks/no7PushPop.js
@@ -0,0 +1,14 @@
+const people = [
+  name1 = "ram",
+  age1 = 18,
+  name2 = "sam",
+  age2 = 19,
+  name3 = "hari",
+  age3 = 20,
+];
+console.log(people);
+people.push(name4 = "ganesh", age4 = 21);
+console.log(people);
+people.pop();
+console.log(people);
+
