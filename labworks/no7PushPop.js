3a9e8e5ef1e563f015e655d694e3a8e5552b098f
bishnulimbu <subashlimbu987654321@gmail.com>
2024-01-07 16:13:31 +0545

    corrected the code with array with objects

--- labworks/no7PushPop.js
+++ labworks/no7PushPop.js
@@ -1,13 +1,18 @@
+// const people = [
+//   name1 = "ram",
+//   age1 = 18,
+//   name2 = "sam",
+//   age2 = 19,
+//   name3 = "hari",
+//   age3 = 20,
+// ];
 const people = [
-  name1 = "ram",
-  age1 = 18,
-  name2 = "sam",
-  age2 = 19,
-  name3 = "hari",
-  age3 = 20,
-];
+  {name : 'ram' ,age: 16 },
+  {name : 'sam' ,age: 17 },
+  {name : 'hari' ,age: 18 },
+]
 console.log(people);
-people.push(name4 = "ganesh", age4 = 21);
+people.push({name : "ganesh", age : 21});
 console.log(people);
 people.pop();
 console.log(people);
