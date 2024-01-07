c5b9824e613a0966118dffd27587feaf631ea68b
bishnulimbu <subashlimbu987654321@gmail.com>
2024-01-07 06:53:29 +0545

    asdfasd

---
+++ FourthSem/array/array2.js
@@ -0,0 +1,7 @@
+const numbers = [
+  1,2,3,4,5
+];
+console.log(numbers[0],numbers[1],numbers[2],numbers[3],numbers[4]);
+
+numbers.toString();
+console.log(numbers[0],numbers[1],numbers[2],numbers[3],numbers[4]);
