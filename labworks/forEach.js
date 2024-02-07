4302b82edfd9374b6cd792a8670a2d0096d8c299
bishnulimbu <subashlimbu987654321@gmail.com>
2024-02-07 18:38:27 +0545

    dff

---
+++ labworks/forEach.js
@@ -0,0 +1,7 @@
+//Write a javascript program to create an array of numbers and use forEach() to print all the even numbers in the array.
+
+const num = [1,2,3,4,5,6,7,8,9,10];
+
+num.forEach(function (rem){
+  console.log(if(rem % 2= 0)rem);
+});
