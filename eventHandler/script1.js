58a35e13b6f1758fd34789672aa4728ea8af171f
bishnulimbu <subashlimbu987654321@gmail.com>
2024-02-08 09:49:08 +0545

    dfdf

--- eventHandler/script1.js
+++ eventHandler/script1.js
@@ -1,5 +1,12 @@
-const heading = document.queryselector('buttonHeading');
+const heading = document.querySelector('#buttonHeading');
 const button = document.querySelector('#button1')
 
 
 //dom event and event handling
+
+button.addEventListener('click', function(event) {
+  heading.style.color = 'red';
+  heading.style.fontSize = '70px';
+  console.log('button clicked')
+
+});
