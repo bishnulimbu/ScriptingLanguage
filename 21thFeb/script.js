9d8d73b7126c162387ff5e1f934048daf1de8544
bishnulimbu <subashlimbu987654321@gmail.com>
2024-02-21 07:37:39 +0545

    dfdf

--- 21thFeb/script.js
+++ 21thFeb/script.js
@@ -1,2 +1,5 @@
 // let username = document.getElementById("firstname", "middlename", "lastname");
-let username = document.getElementByClassName("username");
+// let username = document.getElementByClassName("username");
+// console.log(username);
+let usernames = document.getElementsByClassName("username");
+console.log(usernames[0]);
