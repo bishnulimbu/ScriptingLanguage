e31a58790c7dcb86a8e2b591f02692dc6534752a
bishnulimbu <subashlimbu987654321@gmail.com>
2024-05-04 14:25:30 +0545

    dfdf

---
+++ practice/newOne/smallest.js
@@ -0,0 +1,6 @@
+// let a = [1, 2, 3, 4, 5, 6];
+let a = [6, 5, 4, 3, 2, 1];
+a.sort();
+// console.log(a.sort());
+console.log("smallest is:" + a[0]);
+console.log("greatest is:" + a[a.length - 1]);
